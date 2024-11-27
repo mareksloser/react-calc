68781e588ed0afbf8e9b4fe5136aceee3a1dbbf0
msloser <marek.sloser@nakit.cz>
2024-11-27 13:00:16 +0100

    Remove keyboard element

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import Calc from "./Calc";
 
 function App() {
   return (
-    <div className="calc">
-
-      <div className="calc__keyboard" style={{ width: "300px", margin: "0 auto", display: "grid" }}>
+    <div className="calc" style={{ width: "300px", margin: "0 auto", display: "grid" }}>
         <Calc />
-      </div>
     </div>
   );
 }
